d34b4c8d5668ca470308625d12096b4b4c867ef0
XERXEZAI <xerxez.in@gmial.com>
2026-06-27 11:35:54 +0530

    Remove breadcrumb from Services and Portfolio pages

--- src/page/ProjectPage.tsx
+++ src/page/ProjectPage.tsx
@@ -1,11 +1,9 @@
-﻿import BreadcrumbSection from "../components/breadcrumb/BreadcrumbSection";
-import CustomLayout from "../components/layout/CustomLayout";
+﻿import CustomLayout from "../components/layout/CustomLayout";
 import ProjectMainSection from "../components/project/ProjectMainSection";
 
 const ProjectPage = () => {
   return (
     <CustomLayout>
-        <BreadcrumbSection title="Our Case Study" />
         <ProjectMainSection />
     </CustomLayout>
   );
--- src/page/ServicePage.tsx
+++ src/page/ServicePage.tsx
@@ -1,4 +1,3 @@
-import BreadcrumbSection from "../components/breadcrumb/BreadcrumbSection";
 import CounterSection from "../components/counter/CounterSection";
 import CustomLayout from "../components/layout/CustomLayout";
 import ServiceSection3 from "../components/service/ServiceSection3";
@@ -6,7 +5,6 @@ import ProcessTimeline from "../components/timeline/ProcessTimeline";
 
 const ServicePage = () => (
   <CustomLayout>
-    <BreadcrumbSection title="Our Services" />
     <ServiceSection3 mainSection />
     <ProcessTimeline />
     <CounterSection variant="style-3" />
