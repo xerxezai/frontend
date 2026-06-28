ae6785dfc1aab30c52991d78bbaf43a958117da3
XERXEZAI <xerxez.in@gmial.com>
2026-06-28 13:43:31 +0530

    remove map section from Contact page

--- src/page/ContactPage.tsx
+++ src/page/ContactPage.tsx
@@ -1,13 +1,11 @@
 import ContactSection2 from "../components/contact/ContactSection2";
 import ContactTrustBar from "../components/contact/ContactTrustBar";
-import MapSection from "../components/contact/MapSection";
 import CustomLayout from "../components/layout/CustomLayout";
 
 const ContactPage = () => (
   <CustomLayout>
     <ContactSection2 />
     <ContactTrustBar />
-    <MapSection />
   </CustomLayout>
 );
 
